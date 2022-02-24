e1e9a8d1384c8cf79ac0d45c2492ee2b552ff0d5
Manal Soava <manal.hibat.allah@gmail.com>
2022-02-24 10:52:38 +0100

    authentification

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRouter = require("./routes/user");
+
+const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://manal:ilyan2020@cluster0.owdzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", userRouter);
+
+module.exports = app;
